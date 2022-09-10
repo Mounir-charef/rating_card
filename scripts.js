881138c5d1dce12a36b449dfc552f5e8dfd96d36
Mounir <polomnr1@gmail.com>
2022-09-10 15:36:02 +0100

    finished

--- scripts.js
+++ scripts.js
@@ -20,7 +20,6 @@ function removeHover(){
 
 butts.forEach((btn) => {
     btn.addEventListener('click',()=>{
-        console.log(btn);
         rate.textContent = btn.textContent;
         removeHover()
         addHover(btn.dataset.tabindex);
